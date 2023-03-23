2b478a6f1999cf3468dcdda7ec06baa88ea12f4a
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2023-03-23 11:02:56 +0100

    RefectoredCodeWithNewTests
    RefectoredCodeWithNewTests

--- code_after_refactoring/main.py
+++ code_after_refactoring/main.py
@@ -4,6 +4,9 @@ from pay.payment import pay_order
 
 def main():
     # Test card number: 1249190007575069
+    card = input("Please enter your card number: ")
+    month = int(input("Please enter the card expiry month: "))
+    year = int(input("Please enter the card expiry year: "))
     order = Order()
     order.line_items.append(LineItem(name="Shoes", price=100_00, quantity=2))
     order.line_items.append(LineItem(name="Hat", price=50_00))
--- code_after_refactoring/pay/tests/test_processor.py
+++ code_after_refactoring/pay/tests/test_processor.py
@@ -1,53 +1,53 @@
-from processor import PaymentProcessor
+import os
+from datetime import date
+
 import pytest
+from dotenv import load_dotenv
+from credit_card import CreditCard
+from processor import PaymentProcessor, luhn_checksum
 
-# For testing purposes the API KEY is placed in this test file.
-# Normally this is never done but this makes learning and practicing unit testing easier
-API_KEY = "6cfb67f3-6281-4031-b893-ea85db0dce20"
+load_dotenv()
 
-# Create a test function with return value None
-def test_api_key_invalid() -> None:
-    # Using the with statement it is possible to declaire what type or error we are expecting.
-    # In this situation if the API KEY is invalid we are expecting to get a ValueError.
+API_KEY = os.getenv("API_KEY") or ""
+
+CC_YEAR = date.today().year + 2
+
+
+@pytest.fixture
+def payment_processor() -> PaymentProcessor:
+    return PaymentProcessor(API_KEY)
+
+
+def test_invalid_api_key() -> None:
     with pytest.raises(ValueError):
-        # Create a processor with an emty API KEY string meaning this API KEY will be invalid
-        processor = PaymentProcessor("")
-        processor.charge("1249190007575069", 12, 2024, 100)
+        card = CreditCard("1249190007575069", 12, CC_YEAR)
+        PaymentProcessor("").charge(card, 100)
 
-# Create a test function with return value None 
-def test_card_valid_date() -> None:
-    # Create a processor with valid API KEY and valid parameters
-    processor = PaymentProcessor(API_KEY)
-    processor.charge("1249190007575069", 12, 2024, 100)
 
-# Create a test function with return value None   
-def test_card_invalid_date() -> None:
+def test_card_number_valid_date(payment_processor: PaymentProcessor) -> None:
+    card = CreditCard("1249190007575069", 12, CC_YEAR)
+    assert payment_processor.validate_card(card)
+
+
+def test_card_number_invalid_date(payment_processor: PaymentProcessor) -> None:
+    card = CreditCard("1249190007575069", 12, 1900)
+    assert not payment_processor.validate_card(card)
+
+
+def test_card_number_invalid_luhn() -> None:
+    assert not luhn_checksum("1249190007575068")
+
+
+def test_card_number_valid_luhn() -> None:
+    assert luhn_checksum("1249190007575069")
+
+
+def test_charge_card_valid(payment_processor: PaymentProcessor) -> None:
+    card = CreditCard("1249190007575069", 12, CC_YEAR)
+    payment_processor.charge(card, 100)
+
+
+def test_charge_card_invalid(payment_processor: PaymentProcessor) -> None:
     with pytest.raises(ValueError):
-        # Create a processor with valid API KEY but invalid date parameters
-        processor = PaymentProcessor(API_KEY)
-        processor.charge("1249190007575069", 12, 1900, 100)
-
-# Right now the luhn_checksum() tests are being run in this file but in truth these are seperate from the payment processor.
-# Better is to seperate these from the payment processor object to make the tests simpler
-# But for practice I keep these here for now
-
-def test_card_number_invalid_luhn():
-    # Create a processor with valid API KEY but invalid card number according to the luhn_checksum()
-    payment_processor = PaymentProcessor(API_KEY)
-    assert not payment_processor.luhn_checksum("1249190007575068")
-
-def test_card_number_valid_luhn():
-    # Create a processor with valid API KEY and valid card number according to the luhn_checksum()
-    payment_processor = PaymentProcessor(API_KEY)
-    assert payment_processor.luhn_checksum("1249190007575069")
-    
-def test_charge_card_valid():
-    # Create a processor with valid API KEY and valid charge parameters
-    payment_processor = PaymentProcessor(API_KEY)
-    payment_processor.charge("1249190007575069", 12, 2024, 100)
-    
-def test_charge_card_invalid():
-    # Create a processor with valid API KEY but invalid charge parameters to make sure this raises a ValueError
-    with pytest.raises(ValueError):
-        payment_processor = PaymentProcessor(API_KEY)
-        payment_processor.charge("1249190007575068", 12, 2024, 100)
+        card = CreditCard("1249190007575068", 12, CC_YEAR)
+        payment_processor.charge(card, 100)
