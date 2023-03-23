db59c5708cdca47f7ae0c6b80e3d20b2230d42af
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2023-03-23 09:59:08 +0100

    PaymentProcessorRefactoredAndTested
    PaymentProcessorRefactoredAndTested

--- code_after_refactoring/pay/payment.py
+++ code_after_refactoring/pay/payment.py
@@ -1,13 +1,21 @@
+from asyncio import Protocol
 from order import Order
-from processor import PaymentProcessor
 
+# Defining a PaymentProcessor protocol will make it much easier to introduce a mock version of this while testing
+class PaymentProcessor(Protocol):
+    def charge(self, card: str, month: int, year: int, amount: int) -> None:
+        """Charges the card with the amount."""
+    
 
-def pay_order(order: Order):
+# Refactoring this code includes adding dependency injection for the pay_order() function.
+# This means providing a function in this case pay_order() with the objects that it needs.
+# It will make testing easier and gives us more control over how the PaymentProcessor is created.
+# It's also possible to combine dependency injection with dependency inversion then it is possible to replace the PaymentProcessor entirely without having to change anything about the pay_order() function
+def pay_order(order: Order, processor: PaymentProcessor):
     if order.total == 0:
         raise ValueError("Can't pay an order with total 0.")
     card = input("Please enter your card number: ")
     month = int(input("Please enter the card expiry month: "))
     year = int(input("Please enter the card expiry year: "))
-    payment_processor = PaymentProcessor("6cfb67f3-6281-4031-b893-ea85db0dce20")
-    payment_processor.charge(card, month, year, amount=order.total)
+    processor.charge(card, month, year, amount=order.total)
     order.pay()
